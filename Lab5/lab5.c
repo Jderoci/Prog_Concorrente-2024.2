690a2e70ee0da46c3067b2335c2b6bbdc9485803
Julia Deroci <79822930+Jderoci@users.noreply.github.com>
2024-09-20 02:26:01 -0300

    Update lab5.c

--- Lab5/lab5.c
+++ Lab5/lab5.c
@@ -1,84 +1,82 @@
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
-long int soma = 0;     // variavel compartilhada entre as threads
-pthread_mutex_t mutex; // variavel de lock para exclusao mutua
-pthread_cond_t cond;   // variavel de condicao para sincronizacao
-int flag = 0;          // flag para controlar a execucao
+long int soma = 0;     // variável compartilhada entre as threads
+pthread_mutex_t mutex; // variável de lock para exclusão mútua
+pthread_cond_t cond;   // variável de condição para sincronização
+int flag = 0;          // flag para controlar a execuçãao
 
-// funcao executada pela thread principal
+// Função executada pela thread principal 
 void *ExecutaTarefa(void *arg) {
-  printf("Thread de trabalho esta executando...\n");
+  printf("Thread ExecutaTarefa está executando...\n");
 
   for (int i = 0; i < 100000; i++) {
-    // --entrada na SC
+    // Entrada na seção crítica
     pthread_mutex_lock(&mutex);
+    soma++; 
 
-    soma++; // incrementa a variavel compartilhada
-
-    if (soma % 10 == 0) { // verifica se 'soma' e multiplo de 10
-      flag = 1; // seta a flag para indicar que um multiplo de 10 foi encontrado
-      pthread_cond_signal(&cond);       // sinaliza para a thread extra
-      pthread_cond_wait(&cond, &mutex); // espera a thread extra liberar
+    // Verificando se 'soma' é múltiplo de 10
+    if (soma % 10 == 0) { 
+      flag = 1; // Indica que um multiplo de 10 foi encontrado
+      pthread_cond_signal(&cond);       // Sinaliza para a thread extra
+      pthread_cond_wait(&cond, &mutex); // Espera a thread extra liberar
     }
 
-    // --saida da SC
+    // Saída da seção crítica
     pthread_mutex_unlock(&mutex);
   }
 
-  printf("Thread de trabalho terminou!\n");
+  printf("Thread ExecutaTarefa terminou!\n");
   pthread_exit(NULL);
 }
 
-// funcao executada pela thread extra
+// Função executada pela thread Extra
 void *extra(void *arg) {
   int count = 0;
-  printf("Thread extra esta executando...\n");
+  printf("Thread Extra esta executando...\n");
 
-  while (
-      count <
-      20) { // imprime os 20 primeiros valores de 'soma' que sao multiplos de 10
+  // Imprimindo os 20 primeiros valores de 'soma' que são múltiplos de 10
+  while (count < 20) { 
     pthread_mutex_lock(&mutex);
-
-    while (flag == 0) { // espera um multiplo de 10 ser encontrado
+    // Esperando um múltiplo de 10 ser encontrado
+    while (flag == 0) { 
       pthread_cond_wait(&cond, &mutex);
     }
 
-    // imprime o valor de 'soma'
+    // Printando o valor de 'soma'
     printf("soma = %ld\n", soma);
     count++;
 
-    flag = 0;                   // reseta a flag
-    pthread_cond_signal(&cond); // sinaliza para a thread principal continuar
-
+    flag = 0;                   // Reseta a flag
+    pthread_cond_signal(&cond); // Sinaliza para a thread principal continuar
     pthread_mutex_unlock(&mutex);
   }
 
-  printf("Thread extra terminou!\n");
+  printf("Thread Extra terminou!\n");
   pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
   pthread_t tid[2]; // identificadores das threads no sistema
 
-  // --inicilaiza o mutex e a condicao
+  // Inicilaizando o mutex e a condição
   pthread_mutex_init(&mutex, NULL);
   pthread_cond_init(&cond, NULL);
 
-  //--cria a thread de trabalho
+  // Criando a thread ExecutaTarefa
   if (pthread_create(&tid[0], NULL, ExecutaTarefa, NULL)) {
     printf("--ERRO: pthread_create()\n");
     exit(-1);
   }
 
-  //--cria a thread extra
+  // Criando a thread Extra
   if (pthread_create(&tid[1], NULL, extra, NULL)) {
     printf("--ERRO: pthread_create()\n");
     exit(-1);
   }
 
-  //--espera as threads terminarem
+  // Esperando as threads terminarem
   for (int t = 0; t < 2; t++) {
     if (pthread_join(tid[t], NULL)) {
       printf("--ERRO: pthread_join()\n");
@@ -86,7 +84,7 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  //--finaliza o mutex e a condicao
+  // Finalizando o mutex e a condição
   pthread_mutex_destroy(&mutex);
   pthread_cond_destroy(&cond);
 
