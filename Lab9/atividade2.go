75ce41c0978c6567e860761f116b935bd96b2ecf
Julia Deroci <79822930+Jderoci@users.noreply.github.com>
2024-11-07 21:31:08 -0300

    Update atividade2.go

--- Lab9/atividade2.go
+++ Lab9/atividade2.go
@@ -1,1 +1,52 @@
+// Canais na aplicação Go
+package main
+
+import (
+	"fmt"
+)
+
+// Goroutine com diálogo
+func tarefa(str chan string) {
+	// Recebe mensagem de `main` e imprime
+	msg := <-str
+	fmt.Println(msg)
+
+	// Envia resposta para `main`
+	str <- "Oi Main, bom dia, tudo bem?"
+
+	// Recebe segunda mensagem de `main` e imprime
+	msg = <-str
+	fmt.Println(msg)
+
+	// Envia resposta final para `main`
+	str <- "Certo, entendido."
+
+	// Finaliza goroutine
+	fmt.Println("finalizando goroutine")
+	str <- "goroutine terminou"
+}
+
+func main() {
+	// Canal para comunicação
+	str := make(chan string)
+
+	// Inicia a goroutine
+	go tarefa(str)
+
+	// Envia primeira mensagem
+	str <- "Olá, Goroutine, bom dia!"
+
+	// Recebe e imprime resposta
+	fmt.Println(<-str)
+
+	// Envia segunda mensagem
+	str <- "Tudo bem! Vou terminar tá?"
+
+	// Recebe e imprime resposta final
+	fmt.Println(<-str)
+
+	// Recebe confirmação de finalização da goroutine
+	<-str
+	fmt.Println("finalizando main")
+}
 
